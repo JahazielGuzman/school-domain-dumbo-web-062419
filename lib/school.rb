5590b17ed95e359255f36d433a25ba1d8c084d9e
Jahaziel Guzman <jahaziel.guzman93@gmail.com>
2019-06-25 17:00:54 -0400

    school model is working

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+
+class School
+	def initialize(name)
+		@name = name
+		@roster = {}
+	end
+
+	def add_student(name, grade)
+		if !@roster.keys.include?(grade)
+			@roster[grade] = []
+		end
+		@roster[grade] << name
+	end
+
+	def roster
+		@roster
+	end
+
+	def grade(grade)
+		@roster[grade]
+	end
+
+	def sort
+		sorted_school = {}
+		@roster.each{|k,v| sorted_school[k] = v.sort}
+		sorted_school
+	end
+end
